156a10b35477c9828bde07a3cbd12baaf36d7318
Idris <franckenstein999@gmail.com>
2024-05-08 09:39:12 -0700

    Update another version

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-sender = ARGV[0].match(/\[from:(.*?)\]/)&.captures&.first
-receiver = ARGV[0].match(/\[to:(.*?)\]/)&.captures&.first
-flags = ARGV[0].match(/\[flags:(.*?)\]/)&.captures&.first
+sender = ARGV[0].match(/\[from:(?<sender>.*?)\]/)[:sender]
+receiver = ARGV[0].match(/\[to:(?<receiver>.*?)\]/)[:receiver]
+flags = ARGV[0].match(/\[flags:(?<flags>.*?)\]/)[:flags]
 
 puts "#{sender},#{receiver},#{flags}"
