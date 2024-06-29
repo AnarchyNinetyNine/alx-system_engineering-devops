8e7bb7cd6d97246abbd8d50eba7a1896938bd907
Idris <franckenstein999@gmail.com>
2024-06-28 18:02:42 -0700

    Task 1: Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,25 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Ensure Python 3.8 is installed
+package { 'python3.8.10':
+  ensure => 'installed',
 }
+
+# Ensure pip for Python 3 is installed
+package { 'python3-pip':
+  ensure => 'installed',
+  require => Package['python3.8.10'],
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug version 2.1.1 using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
