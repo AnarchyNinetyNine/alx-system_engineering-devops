3dfaef019c6d49c6ed5e635cb634d3f7c989e704
Idris <franckenstein999@gmail.com>
2024-06-28 18:06:11 -0700

    Task 1: Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,6 @@
-# Ensure Python 3.8 is installed
-package { 'python3.8.10':
-  ensure => 'installed',
-}
+# Install Flask -v 2.1.0
 
-# Ensure pip for Python 3 is installed
-package { 'python3-pip':
-  ensure => 'installed',
-  require => Package['python3.8.10'],
-}
-
-# Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
-
-# Install Werkzeug version 2.1.1 using pip3
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['flask'],
-}
-
