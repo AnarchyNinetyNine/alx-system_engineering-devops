344ea79f6315f6df764cadf89924a92f9cfd2f63
Idris Elgarrab <franckenstein999@gmail.com>
2024-08-13 19:18:50 -0700

    Task 1: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
 
 """
-    This module provides a function to query the Reddit API
-    and return the number of subscribers for a given subreddit.
+    Function to query subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
 
-    """Returns the number of subscribers for a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
 
-    headers = {'User-Agent': 'Custom'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    CLIENT_ID = "8T_hvDU-VQ9UhrEtEhG4nw"
+    SECRET_KEY = "J2jaV-I742YrkXTMGzQ6S6MjrTtHCQ"
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    base_url = 'https://www.reddit.com/'
+    data = {
+                'grant_type': 'password',
+                'username': 'Big_Confection_4617',
+                'password': 'ArtukBay2019!'
+            }
 
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+    r = requests.post(
+            base_url + 'api/v1/access_token',
+            data=data,
+            headers={'user-agent': 'my_app by Big_Confection_4617'},
+            auth=auth)
+
+    d = r.json()
+
+    base_url = 'https://oauth.reddit.com'
+    token = 'bearer ' + d['access_token']
+    headers = {
+            'Authorization': token,
+            'User-Agent': 'my_app by Big_Confection_4617'}
+    response = requests.get(
+            base_url + "/r/{}/about.json".format(subreddit),
+            headers=headers,
+            allow_redirects=False)
+
+    if response.status_code != 404:
+        return response.json().get('data').get('subscribers')
     else:
         return 0
